402c2b5ff62e2d4c92a045f3d337c9f29ac2b718
genieblockchain <genieblockchain@gmail.com>
2021-12-20 00:30:41 +0900

    Post related to The Cardano Node Upgrade 1.31.0

--- src/components/sponsor-button/index.jsx
+++ src/components/sponsor-button/index.jsx
@@ -5,16 +5,16 @@ import './index.scss'
 export const SponsorButton = ({ sponsorId }) => (
   <div className="sponsor-button">
     <a
-      className="bmc-button"
+      className="stake-button"
       target="_blank"
       rel="noopener noreferrer"
-      href={`https://www.buymeacoffee.com/${sponsorId}`}
+      href={`https://adapools.org/pool/a2d7c530964b52d333cc664b1fc5acb697f1e3281787af54b021215e`}
     >
       <img
         src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg"
         alt="Buy me a coffee"
       />
-      <span>Buy me a coffee</span>
+      <span>Stake With Genie 지니 풀에서 스테이킹</span>
     </a>
   </div>
 )
